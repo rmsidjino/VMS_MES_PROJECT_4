f8ff3303693c25ca1ea5b774594c54360a7f277c
훈의 홍 <asdfzxcv5621@naver.com>
2021-12-29 21:05:48 +0900

    12/29일 모두 다시 빌드 성공 확인 후 푸시

---
+++ MESDTO/EquipmentVO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MESDTO
+{
+    public class EquipmentVO
+    {
+        public string SITE_ID { get; set; }
+        public string LINE_ID { get; set; }
+        public string EQP_ID { get; set; }
+        public string EQP_MODEL { get; set; }
+        public string EQP_TYPE { get; set; }
+        public string EQP_GROUP { get; set; }
+        public string SIM_TYPE { get; set; }
+        public string PRESET_ID { get; set; }
+        public string DISPATCHER_ID { get; set; }
+        public string EQP_STATE { get; set; }
+        public int EQP_STATE_CODE { get; set; }
+        public DateTime STATE_CHANGE_TIME { get; set; }
+        public string AUTOMATION { get; set; }
+        public string MODIFIER { get; set; }
+        public DateTime MODIFIER_DATE { get; set; }
+
+    }
+}
---
+++ MES_WEB_API/Controllers/EquipmentController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MESDTO;
+using MES_WEB_API.Models;
+
+
+namespace MES_WEB_API.Controllers
+{
+    [RoutePrefix("api/Equipment")]
+    public class EquipmentController : ApiController
+    {
+        // GET: https://localhost:44332/api/Equipment/Equipments
+        [HttpGet]
+        [Route("Equipments")]
+        public List<EquipmentVO> GetAllEquipment()
+        {
+            EquipmentDAC db = new EquipmentDAC();
+            return db.GetAllEquipment();
+        }
+
+        //Post : https://localhost:44332/api/Equipment/InsertEquipment/{id}
+        [HttpPost]
+        [Route("InsertEquipment/{id}")]
+        public IHttpActionResult InsertEquipment(EquipmentVO equipment)
+        {
+            Message msg = new Message();
+
+            EquipmentDAC db = new EquipmentDAC();
+            bool result = db.InsertEquipment(equipment);
+
+            if (result)
+            {
+                msg.IsSuccess = true;
+                msg.ResultMessage = "성공적으로 등록되었습니다.";
+            }
+            else
+            {
+                msg.IsSuccess = false;
+                msg.ResultMessage = "등록 중 오류가 발생했습니다.";
+            }
+            return Ok(msg);
+        }
+        //GET : https://localhost:44332/api/Equipment/DeleteEquipment/{id}
+        [HttpGet]
+        [Route("DeleteEquipment/{id}")]
+        public IHttpActionResult DeleteEquipment(string equipmentID)
+        {
+            Message msg = new Message();
+
+            EquipmentDAC db = new EquipmentDAC();
+            bool result = db.DeleteEquipment(equipmentID);
+
+            if (result)
+            {
+                msg.IsSuccess = true;
+                msg.ResultMessage = "성공적으로 삭제되었습니다.";
+            }
+            else
+            {
+                msg.IsSuccess = true;
+                msg.ResultMessage = "삭제 중 오류가 발생했습니다.";
+            }
+            return Ok(msg);
+        }
+        //GET : https://localhost:44332/api/Equipment/{id}
+        [HttpGet]
+        [Route("{id}")]
+        public IHttpActionResult GetEquipmentInfo(string equipmentID)
+        {
+            Message<EquipmentVO> msg = new Message<EquipmentVO>();
+
+            EquipmentDAC db = new EquipmentDAC();
+            EquipmentVO product = db.GetEquipmentInfo(equipmentID);
+
+            if (product != null)
+            {
+                msg.IsSuccess = true;
+                msg.ResultMessage = "성공적으로 조회되었습니다.";
+                msg.Data = product;
+            }
+            else
+            {
+                msg.IsSuccess = true;
+                msg.ResultMessage = "해당하는 제품이 없습니다.";
+                msg.Data = null;
+            }
+            return Ok(msg);
+        }
+
+    }
+}
