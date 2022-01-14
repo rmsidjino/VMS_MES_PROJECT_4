5cd9e630a1846def8046bf590e39986f72119960
JungJinho <JungJinho@DESKTOP-JHGN0KT>
2022-01-14 21:36:16 +0900

    공통코드 관리 추가, 업데이트 예시 frmCommon 참고

--- MES_WEB_API/Models/CommonDAC.cs
+++ MES_WEB_API/Models/CommonDAC.cs
@@ -90,5 +90,90 @@ where category in ('{category}')";
                 return null;
             }
         }
+
+        public List<CommonVO> GetCommonList()
+        {
+            string sql = $@"select CCODE, CNAME, CCATEGORY from Common_Code";
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    return Helper.DataReaderMapToList<CommonVO>(cmd.ExecuteReader());
+                }
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine(err.Message);
+                return null;
+            }
+        }
+
+        public bool InsertCommon(CommonVO com)
+        {
+            string sql = @"insert into COMMON_CODE (CCODE,CNAME,CCATEGORY) values(@CCODE,@CNAME,@CCATEGORY)";
+
+            try
+            {
+
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+
+                    cmd.Parameters.AddWithValue("@CCODE", com.CCODE);
+                    cmd.Parameters.AddWithValue("@CNAME", com.CNAME);
+                    cmd.Parameters.AddWithValue("@CCATEGORY", com.CCATEGORY);
+
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception err)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCommon(string ccode)
+        {
+            string sql = @"Delete from COMMON_CODE where CCODE = @CCODE ";
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+
+                    cmd.Parameters.AddWithValue("@CCODE", ccode);
+
+                    int iRowAffect = cmd.ExecuteNonQuery();
+
+                    return (iRowAffect > 0);
+                }
+            }
+            catch (Exception err)
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateCommon(CommonVO com)
+        {
+            string sql = "Update COMMON_CODE set CNAME=@CNAME, CCATEGORY=@CCATEGORY where CCODE=@CCODE";
+
+            try
+            {
+                
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@CCODE", com.CCODE);
+                cmd.Parameters.AddWithValue("@CNAME", com.CNAME);
+                cmd.Parameters.AddWithValue("@CCATEGORY", com.CCATEGORY);
+
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine(err.Message);
+                return false;
+            }
+        }
     }
 }
---
+++ VMS_MES_PROJECT_4/PopupCommon.cs
@@ -0,0 +1,99 @@
+﻿using MESDTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VMS_MES_PROJECT_4
+{
+    public partial class PopupCommon : Form
+    {
+        ServiceHelp srv = new ServiceHelp("");
+        MESDTO.Message msg;
+        CommonVO cItem;
+        List<CommonVO> com;
+        bool update = false;
+        public PopupCommon()
+        {
+            InitializeComponent();
+        }
+
+        public PopupCommon(CommonVO cItem)
+        {
+            InitializeComponent();
+            this.cItem = cItem;
+            update = true;
+            txtComCode.Enabled = false;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void btnOK_Click(object sender, EventArgs e)
+        {
+            //유효성 체크
+            StringBuilder sb = new StringBuilder();
+            if (txtComCode.Text.Length < 1)
+            {
+                sb.AppendLine("- 공통코드ID을 입력하세요.");
+            }
+            if (txtComName.Text.Length < 1)
+            {
+                sb.AppendLine("- 공통코드이름을 입력하세요.");
+            }
+            if (sb.ToString().Length > 1)
+            {
+                MessageBox.Show(sb.ToString());
+                return;
+            }
+            CommonVO com = new CommonVO
+            {
+                CCODE = txtComCode.Text,
+                CNAME = txtComName.Text,
+                CCATEGORY = cboComGroup.SelectedItem.ToString()
+            };
+            if (update)
+            {
+                msg = await srv.PostAsyncNone("api/Common/UpdateCommon", com);
+            }
+            else
+            {
+                msg = await srv.PostAsyncNone("api/Common/InsertCommon", com);
+            }
+
+            MessageBox.Show(msg.ResultMessage);
+            if (msg.IsSuccess)
+            {
+                //btnSearch.PerformClick();
+            }
+            this.Close();
+
+            
+            
+        }
+
+        private async void PopupCommon_Load(object sender, EventArgs e)
+        {
+            com = null;
+            com = await srv.GetListAsync($"api/Common/Commons", com);
+
+            var cCategory = com.Select(x => x.CCATEGORY).Distinct();
+            cboComGroup.DataSource = cCategory.ToList();
+
+            if (update)
+            {
+                txtComCode.Text = cItem.CCODE;
+                txtComName.Text = cItem.CNAME;
+                cboComGroup.SelectedItem = cItem.CCATEGORY;
+            }
+        }
+
+    }
+}
